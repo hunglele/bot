a5bb0f276fee57fe30ad4da147ba91b75e4d58f1
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2018-09-21 23:03:26 +0700

    Fix not end session when say "Pp"

--- src/Chatible/index.js
+++ src/Chatible/index.js
@@ -14,7 +14,7 @@ const ChatfuelAPI = new Chatfuel();
 export default async (user) => {
     const idCouple = cache.get(user.senderId)
     if (idCouple) {
-        if (user.msg === "pp") return await bye(user.senderId, idCouple)
+        if (user.msg.toLowerCase() === "pp") return await bye(user.senderId, idCouple)
         return await ChatfuelAPI.sendText(idCouple, user.msg);
     }
     const userDb = await findUser(user.senderId);
@@ -29,7 +29,7 @@ export default async (user) => {
     } else if (userDb.status === 1) {
         return await ChatfuelAPI.sendText(user.senderId, "Chờ chút. Yêu cầu của bạn chắc bây giờ không có ai cạ. Thử huỷ đi chọn lại hoặc chờ đợi nhé")
     } else if (userDb.idCouple) {
-        if (user.msg === "pp") return await bye(user.senderId, userDb.idCouple)
+        if (user.msg.toLowerCase() === "pp") return await bye(user.senderId, userDb.idCouple)
         cache.put(user.senderId, userDb.idCouple)
         return await ChatfuelAPI.sendText(userDb.idCouple, user.msg);
     }

e16fd2249c5a4241d8398122039b7a2d819d530e
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2018-09-26 20:29:35 +0700

    Added status routes to debug

--- src/app.js
+++ src/app.js
@@ -16,7 +16,19 @@ app.get('/ping', (_, res) => res.send("pong"))
 
 app.use('/chatible', Chatible)
 app.use('/get', GetMessage)
-
+app.get('/status', (req, res) => {
+    res.json({
+        mongodb_url: process.env.MONGODB_URI,
+        bot: {
+            TOKEN_CHATFUEL: process.env.TOKEN_CHATFUEL,
+            BOT_ID: process.env.BOT_ID,
+            TEXT_BLOCK: process.env.TEXT_BLOCK,
+            IMAGE_BLOCK: process.env.IMAGE_BLOCK,
+            VOICE_BLOCK: process.env.VOICE_BLOCK,
+        },
+        PORT: process.env.PORT || 3000,
+    })
+})
 
 function runServer(port = process.env.PORT || 3000, done) {
     /* 3000 is port default */
